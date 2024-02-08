cc93e7eb6dae8ed526183f58bef0a7c0cec946b2
Dunkhan <drunken4ever@gmail.com>
2024-02-08 10:30:11 +0100

    implement gpt api call

--- contentScript.js
+++ contentScript.js
@@ -4,7 +4,7 @@ chrome.runtime.onMessage.addListener((request) => {
       console.log("message");
     if (request.prompt) {
       const paragraphs = document.querySelectorAll('p');
-      const max = 10;
+      const max = 1;
       let counter = 0;
       paragraphs.forEach(function(paragraph) {
           if(counter >= max) {
@@ -12,14 +12,60 @@ chrome.runtime.onMessage.addListener((request) => {
           }
           counter++;
           //console.log(paragraph);
-        paragraph.innerText = getRewrittenText(paragraph.innerText, request.prompt);
+        paragraph.innerText = getRewrittenText(paragraph.innerText, request.prompt, request.apiKey);
       });
         console.log("done");
     }
   }
 );
 
-function getRewrittenText(originalText, prompt) {
+
+function getRewrittenText(originalText, prompt, apiKey) {
+    //let actualPrompt = String.format(prompt, originalText);
+    let actualPrompt = "Say Hi";
+    console.log(actualPrompt);
+    fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer '+ apiKey,
+        },
+        body: JSON.stringify({
+            "model": "gpt-3.5-turbo-0125",
+            "messages": [{"role": "user", "content": actualPrompt}],
+            "temperature": 0.5
+        }),
+    })
+        .then(
+            function(response) {
+                if (response.status !== 200) {
+                    console.log(response);
+                    return;
+                }
+
+                // Examine the text in the response
+                response.json().then(function(data) {
+                    console.log("Success", data);
+                    alert("success");
+                });
+            }
+        )
+        .catch(function(err) {
+            console.log('Fetch Error :-S', err);
+        });
   return originalText.replaceAll("a", "🔥");
 }
+
+if (!String.format) {
+    String.format = function(format) {
+        let args = Array.prototype.slice.call(arguments, 1);
+        return format.replace(/{(\d+)}/g, function(match, number) {
+            return typeof args[number] != 'undefined'
+                ? args[number]
+                : match
+                ;
+        });
+    };
+}
+
 
--- popup.js
+++ popup.js
@@ -1,6 +1,15 @@
 const rewriteButton = document.getElementById('rewriteTextBtn');
 const selector = document.getElementById('promptSelector');
+const apiKeyInput = document.getElementById('apiKeyInput');
 const bkg = chrome.extension.getBackgroundPage();
+
+prompts = [
+    "none",
+    "reword the text {0} paragraph by paragraph, as though written by someone with a third grade writing ability",
+    "reword the text {0} paragraph by paragraph, as though written by someone with a third grade writing ability",
+    "reword the text {0} paragraph by paragraph, as though written by someone with a third grade writing ability",
+    "reword the text {0} paragraph by paragraph, as though written by someone with a third grade writing ability",
+]
 
 document.addEventListener('DOMContentLoaded', function() {
   rewriteButton.addEventListener('click', replaceText)
@@ -10,6 +19,10 @@ function replaceText(event) {
   bkg.console.log('click');
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var selectedPrompt = selector.value;
-    chrome.tabs.sendMessage(tabs[0].id, {prompt: selectedPrompt});
+    bkg.console.log('prompt ', selectedPrompt, prompts[selectedPrompt]);
+    if(selectedPrompt != 0) {
+      chrome.tabs.sendMessage(tabs[0].id, {prompt: prompts[selectedPrompt], apiKey: apiKeyInput.value});
+    }
   });
 }
+
